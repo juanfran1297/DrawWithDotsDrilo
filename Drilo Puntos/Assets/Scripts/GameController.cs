01a0df3c8e4d33d92304119b51b9f577d7ab5a0b
juanfran1297 <jfran191297@gmail.com>
2020-07-08 10:09:27 +0200

    Initial commit

---
+++ Drilo Puntos/Assets/Scripts/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<GameObject> puntos;
+
+    public bool marcado1;
+    public bool marcado2;
+
+    //Esto es para hacer pruebas
+    public Text textoDePrueba;
+
+    public void Comprobar()
+    {
+        if(puntos[0].GetComponent<PuntoController>().marcado == true && puntos[1].GetComponent<PuntoController>().marcado == true)
+        {
+            Debug.Log("Orden correcto, ¡A dibujar!");
+            textoDePrueba.text = "Orden correcto, ¡A dibujar!";
+            puntos[0].GetComponent<PuntoController>().enabled = false;
+            //Prueba visual de que funciona
+            puntos[0].GetComponent<SpriteRenderer>().color = Color.red;
+            ///////////////////////////////////////////////////////////
+            puntos[1].GetComponent<PuntoController>().enabled = false;
+            //Prueba visual de que funciona
+            puntos[1].GetComponent<SpriteRenderer>().color = Color.red;
+            ///////////////////////////////////////////////////////////
+            puntos.Remove(puntos[0]);
+            puntos.Remove(puntos[0]);
+            Reiniciar();
+        }
+        else
+        {
+            Debug.Log("No está en el orden correcto");
+            textoDePrueba.text = "No está en el orden correcto";
+            Reiniciar();
+        }
+    }
+
+    public void ComprobarPrimero()
+    {
+        if(marcado1 == true && marcado2 == false)
+        {
+            if(puntos[0].GetComponent<PuntoController>().marcado == false)
+            {
+                Debug.Log("No se ha cogido el primero correctamente");
+                textoDePrueba.text = "No se ha cogido el primero correctamente";
+                Reiniciar();
+            }
+        }
+    }
+
+    private void Reiniciar()
+    {
+        for(int i = 0; i < puntos.Count; i++)
+        {
+            puntos[i].GetComponent<PuntoController>().marcado = false;
+            marcado1 = false;
+            marcado2 = false;
+        }
+    }
+}
---
+++ Drilo Puntos/Assets/Scripts/PuntoController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuntoController : MonoBehaviour
+{
+    public bool marcado;
+    private void Start()
+    {
+        marcado = false;
+    }
+    private void OnMouseEnter()
+    {
+        if(marcado == false)
+        {
+            marcado = true;
+
+            if (FindObjectOfType<GameController>().marcado1 == false)
+            {
+                FindObjectOfType<GameController>().marcado1 = true;
+                FindObjectOfType<GameController>().ComprobarPrimero();
+            }
+            else if (FindObjectOfType<GameController>().marcado1 == true)
+            {
+                FindObjectOfType<GameController>().marcado2 = true;
+                FindObjectOfType<GameController>().Comprobar();
+            }
+        }
+        if(marcado == true)
+        {
+            return;
+        }
+    }
+}
