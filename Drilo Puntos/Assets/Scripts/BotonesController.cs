f06e85b03f64d53feb259db13970b15d94850ea7
juanfran1297 <jfran191297@gmail.com>
2020-07-10 10:35:09 +0200

    Trabajo de moratalla

---
+++ Drilo Puntos/Assets/Scripts/BotonesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonesController : MonoBehaviour
+{
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    public void CargarEscena(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+}
---
+++ Drilo Puntos/Assets/Scripts/Line.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+
+    List<Vector2> points;
+
+    public void UpdateLine(Vector2 mousePos)
+    {
+        if(points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(mousePos);
+            return;
+        }
+
+        //Check if the mouse has moved enough for us to insert new point 
+        if (Vector2.Distance(points.Last(), mousePos) > .03f)
+        {
+            //If it has: Insert point at mouse position
+            SetPoint(mousePos);
+        }
+    }
+
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPosition(points.Count - 1, point);
+
+        if(points.Count > 1)
+            edgeCollider.points = points.ToArray();
+    }
+}
